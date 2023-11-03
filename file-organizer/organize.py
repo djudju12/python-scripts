db42acb01e1998f3e0f940776f6e7306926b0c0a
Jonathan Santos <jonathan.willian321@gmail.com>
2023-11-03 13:01:40 -0300

    feature to create new config file

--- file-organizer/organize.py
+++ file-organizer/organize.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 
-import yaml, os, glob
+import yaml, os, glob, sys
 
 CONFIG_PATH="organizer.yaml"
 LOG_ACTIVE = True
 
-def log(msg: str) -> None:
-    if LOG_ACTIVE:
-        print(msg)
-
 def main():
+    args = shift_args()
+    if len(args) > 0:
+        match args[0]:
+            case "-i" | "--init":
+                init_config()
+                return 
+
+
     config = read_config(CONFIG_PATH)
     for dir, file_patterns in config.items():
         if type(file_patterns) is not list:
@@ -19,6 +23,18 @@ def main():
         for pattern in file_patterns:
             for file in file_list(pattern):
                 move_file(file, dir)
+
+def log(msg: str) -> None:
+    if LOG_ACTIVE:
+        print(msg)
+
+def shift_args() -> list[str]:
+    if len(sys.argv) > 1:
+        return sys.argv[1:]
+    return []
+
+def path_exists(path: str) -> bool:
+    return os.path.exists(path)
 
 def path_not_exists(path: str) -> bool:
     return not os.path.exists(path)
@@ -38,17 +54,27 @@ def move_file(file: str, dst: str) -> None:
 def dir_exists(path: str) -> bool:
     return os.path.exists(path) and os.path.isdir(path)
 
+def init_config() -> None:
+    if path_exists(CONFIG_PATH):
+        log(f"Config file \"{CONFIG_PATH}\" already exists.")
+        if input("Want to overwrite? (y/n): ") != "y":
+            exit(0)
+    
+    with open(CONFIG_PATH, "w") as f:
+        log(f"Creating new config file \"{CONFIG_PATH}\"")
+        yaml.dump({"Documents":"*.pdf"}, f)
+
 def read_config(path: str) -> dict[str, str]:
     # why yml and yaml? oh god
     if path_not_exists(path):
         name, ext = os.path.splitext(path)
-        if ext != "yaml" and ext != "yml":
+        if ext != ".yaml" and ext != ".yml":
             log(f"Invalid config file. \"{path}\" is not a yaml file.")
             exit(1)
 
         # try again
-        ext = "yml" if ext == "yaml" else "yaml"
-        path = f"{name}.{ext}"
+        ext = ".yml" if ext == ".yaml" else ".yaml"
+        path = f"{name}{ext}"
         if path_not_exists(path):
             log(f"Config file \"{path}\" does not exist.")
             exit(1)
